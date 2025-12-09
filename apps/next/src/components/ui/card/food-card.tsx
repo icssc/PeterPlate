0eb53b59e41d47da66760e4a2013301b72285446
Jacob Moy <apixelatedpug@gmail.com>
2025-12-09 14:52:13 -0800

    fix: comment out a comment

--- apps/next/src/components/ui/card/food-card.tsx
+++ apps/next/src/components/ui/card/food-card.tsx
@@ -87,7 +87,7 @@ const FoodCardContent = React.forwardRef<
                   </div> */}
                 </div>
               </div>
-              //TODO: Add user feedback on clicking button (e.g. changing Icon, making it green)
+              {/*//TODO: Add user feedback on clicking button (e.g. changing Icon, making it green) */}
               <button onClick={handleLogMeal}>
                 <CirclePlus/>
               </button>
