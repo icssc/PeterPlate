5467e3018385bd9d951d203090ec70c84cc946cd
Kevin Wu <kevinwu098@gmail.com>
2025-12-10 02:23:42 -0800

    fix: db nonsense (#566)

--- packages/db/test-config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-if (!process.env.TEST_URL) throw new Error("TEST_URL is not set");
-
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "../db/src/schema",
-  dbCredentials: { url: process.env.TEST_URL },
-});
