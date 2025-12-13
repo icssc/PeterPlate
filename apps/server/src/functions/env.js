b00c1b3b68e88c39f2a7d895dc9beada647bf7bd
Kevin Wu <kevinwu098@gmail.com>
2025-12-13 13:59:11 -0800

    fix: env js

--- apps/server/src/functions/env.js
+++ apps/server/src/functions/env.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
 import { z } from "zod";
 
-if (process.env.NODE_ENV === "development") {
-  dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  dotenv.config({
+    path: resolve(__dirname, "../../../../.env"),
+  });
 }
 
 const envSchema = z.object({
