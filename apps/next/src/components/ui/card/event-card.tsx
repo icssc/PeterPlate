61bcc1d05a432dc64554e86b232b2ae957150927
SAMIKA BHAVESH BHATKAR <samikabhatkar@gmail.com>
2026-01-09 11:04:35 -0800

    feat(frontend): edited sx prop to match prev. styles

--- apps/next/src/components/ui/card/event-card.tsx
+++ apps/next/src/components/ui/card/event-card.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { HallEnum } from "@/utils/types";
 import { dateToString, toTitleCase } from "@/utils/funcs";
-import { Dialog, Card, CardContent, Drawer, Box } from "@mui/material";
+import { Dialog, Card, CardContent, Drawer } from "@mui/material";
 import Image from "next/image";
 import EventDialogContent from "../event-dialog-content";
 import OngoingBadge from "../ongoing-badge";
@@ -62,101 +62,40 @@ const EventCardContent = React.forwardRef<
   EventCardContentProps
 >(({ props, ...divProps }, ref) => {
   return (
-    <Box ref={ref} {...divProps}>
-      <Card
-        sx={{
-          cursor: "pointer",
-          transition: "box-shadow 0.3s ease-in-out",
-          "&:hover": {
-            boxShadow: 6,
-          },
-        }}
-      >
-        <CardContent
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-            pt: 3,
-            gap: 1.5,
-            flexWrap: "wrap",
-          }}
-        >
-          <Box
-            sx={{
-              width: { xs: "100%", sm: "300px", md: "200px" },
-              height: "auto",
-              borderRadius: "4px",
-              overflow: "hidden",
-            }}
-          >
-            <Image
-              src={props.imgSrc}
-              alt={props.alt}
-              width={300}
-              height={200}
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </Box>
-
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 0.5,
-              height: "100%",
-              flex: 1,
-            }}
-            id="event-card-content"
-          >
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                gap: 1,
-                alignItems: "center",
-              }}
-            >
-              <Box component="strong" sx={{ fontSize: "1.5rem" }}>
-                {props.name}
-              </Box>
+    <div ref={ref} {...divProps}>
+      <Card className="cursor-pointer hover:shadow-lg transition">
+        <CardContent className="flex items-center h-full w-full pt-6 gap-3 flex-wrap">
+          <Image
+            src={props.imgSrc}
+            alt={props.alt}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full sm:w-[300px] md:w-[200px] h-auto rounded-sm"
+          />
+          <div className="flex flex-col gap-1 h-full" id="event-card-content">
+            <div className="flex flex-row gap-2">
+              <strong className="text-2xl">{props.name}</strong>
               {props.isOngoing && <OngoingBadge />}
-            </Box>
-
-            <Box
-              sx={{
-                color: "#a1a1aa",
-                display: "flex",
-                flexDirection: { xs: "column", sm: "row" },
-                gap: { xs: 0.5, sm: 2 },
-              }}
+            </div>
+            <div
+              className="text-zinc-400 flex flex-col sm:flex-row gap-x-4 gap-y-1"
               id="event-card-subheader"
             >
-              <Box sx={{ display: "flex", gap: 0.5, alignItems: "center" }}>
-                <Clock style={{ stroke: "#a1a1aa" }} size={20} />
-                <Box component="p" sx={{ m: 0 }}>
-                  {dateToString(props.startTime, props.endTime)}
-                </Box>
-              </Box>
-              <Box sx={{ display: "flex", gap: 0.5, alignItems: "center" }}>
-                <MapPinned size={20} />
-                <Box component="p" sx={{ m: 0 }}>
-                  {toTitleCase(HallEnum[props.location])}
-                </Box>
-              </Box>
-            </Box>
-
-            <Box component="p" sx={{ maxWidth: "36rem", m: 0 }}>
-              {props.shortDesc}
-            </Box>
-          </Box>
+              <div className="flex gap-1">
+                <Clock className="stroke-zinc-400" />
+                <p>{dateToString(props.startTime, props.endTime)}</p>
+              </div>
+              <div className="flex gap-1">
+                <MapPinned />
+                <p>{toTitleCase(HallEnum[props.location])}</p>
+              </div>
+            </div>
+            <p className="max-w-xl">{props.shortDesc}</p>
+          </div>
         </CardContent>
       </Card>
-    </Box>
+    </div>
   );
 });
 
@@ -172,11 +111,12 @@ EventCardContent.displayName = "EventCardContent";
  * @param {EventInfo} props - The event data to be displayed. See {@link EventInfo} for detailed property descriptions.
  * @returns {JSX.Element} A React component representing an event card.
  */
-export default function EventCard(props: EventInfo): JSX.Element {
+export default function EventCard(props: EventInfo): React.JSX.Element {
+  // error appears when just using JSX.Element, added React. to fix
   const isDesktop = useMediaQuery("(min-width: 768px)");
   const [open, setOpen] = React.useState(false);
 
-  const handleOpen = () => setOpen(true);
+  const handleOpen = () => setOpen(true); // I couldn't find a MUI diaglog trigger component so handling open state manually
   const handleClose = () => setOpen(false);
 
   if (isDesktop)
@@ -197,10 +137,12 @@ export default function EventCard(props: EventInfo): JSX.Element {
           open={open}
           onClose={handleClose}
           sx={{
+            // custom styles to match previous drawer appearance
             "& .MuiDrawer-paper": {
-              maxHeight: "90vh",
-              borderTopLeftRadius: 16,
-              borderTopRightRadius: 16,
+              borderTopLeftRadius: "10px",
+              borderTopRightRadius: "10px",
+              marginTop: "96px",
+              height: "auto",
             },
           }}
         >
