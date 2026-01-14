3f83a6b8e57881317bf81cbd897a2c21c07c9f38
Jacob Moy <apixelatedpug@gmail.com>
2026-01-14 11:25:32 -0800

    chore: 🔧 remove autogenerated .next directories

--- zotmeal/apps/docs/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BDa0oZ2O8iZMUsSlyv0pMBTwDEPpb/ijyGEr8TV9RuE=\"\n}"
--- zotmeal/apps/web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xIL7ywSsqb92uzJRoP0G1iRGWs2W/oSXkWZ8EkUhEsE=\"\n}"
