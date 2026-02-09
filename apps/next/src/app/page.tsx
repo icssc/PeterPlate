c2b704eb9fa5173d60baf16b3a29ee60681e2cdd
Jason Lin <jkhlin@uci.edu>
2026-02-08 18:45:02 -0800

    fix: 🐛 revert back to original mobile view

--- apps/next/src/app/page.tsx
+++ apps/next/src/app/page.tsx
@@ -45,6 +45,28 @@ export default function Home() {
 
   return (
     <div className="flex flex-col h-screen">
+      {/* <div className="flex-shrink-0 p-3 flex justify-center gap-3 bg-white dark:bg-neutral-900 border-b border-neutral-200 dark:border-neutral-700 ">
+        <button
+          onClick={() => setActiveHall(HallEnum.BRANDYWINE)}
+          className={`px-4 py-2 rounded-md text-sm font-medium transition-colors
+            ${activeHall === HallEnum.BRANDYWINE
+              ? 'bg-blue-600 text-white hover:bg-blue-700'
+              : 'bg-neutral-200 dark:bg-neutral-700 text-neutral-800 dark:text-neutral-200 hover:bg-neutral-300 dark:hover:bg-neutral-600'
+            }`}
+        >
+          Brandywine
+        </button>
+        <button
+          onClick={() => setActiveHall(HallEnum.ANTEATERY)}
+          className={`px-4 py-2 rounded-md text-sm font-medium transition-colors
+            ${activeHall === HallEnum.ANTEATERY
+              ? 'bg-blue-600 text-white hover:bg-blue-700'
+              : 'bg-neutral-200 dark:bg-neutral-700 text-neutral-800 dark:text-neutral-200 hover:bg-neutral-300 dark:hover:bg-neutral-600'
+            }`}
+        >
+          Anteatery
+        </button>
+      </div> */}
       <div className="flex-grow overflow-y-auto">
         {activeHall === HallEnum.BRANDYWINE && (
           <Side hall={HallEnum.BRANDYWINE} toggleHall={toggleHall} />
