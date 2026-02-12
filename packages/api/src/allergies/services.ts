35f3f428a855641ef1f2b12023e7b759b174be18
Pranav Batra <batrap1@uci.edu>
2026-02-11 16:02:25 -0800

    feat(backend): ✨ added procedures for allergies & preferences

---
+++ packages/api/src/allergies/services.ts
@@ -0,0 +1,53 @@
+import { upsert } from "@api/utils";
+import type { Drizzle, InsertAllergy, SelectAllergy } from "@peterplate/db";
+import { userAllergies, users } from "@peterplate/db";
+import { TRPCError } from "@trpc/server";
+import { and, eq } from "drizzle-orm";
+
+/**
+ * Get all user allergies for a given user ID.
+ * Returns empty array if the user has no listed allergies.
+ */
+export async function getAllergies(db: Drizzle, userId: string) {
+  const allergies = await db.query.userAllergies.findMany({
+    where: (ua, { eq }) => eq(ua.userId, userId),
+  });
+  //Return each allergy string
+  return allergies.map((a) => a.allergy);
+}
+
+export async function addAllergy(
+  db: Drizzle,
+  userId: string,
+  allergy: string,
+): Promise<InsertAllergy> {
+  //check if allergy already in table
+  const existing_allergy = await db.query.userAllergies.findFirst({
+    where: (ua, { eq }) => and(eq(ua.userId, userId), eq(ua.allergy, allergy)),
+  });
+
+  if (existing_allergy) {
+    return null;
+  }
+
+  const newAllergy: InsertAllergy = {
+    userId,
+    allergy,
+  };
+
+  await db.insert(userAllergies).values(newAllergy);
+
+  return newAllergy;
+}
+
+export async function deleteAllergy(
+  db: Drizzle,
+  userId: string,
+  allergy: string,
+): Promise<void> {
+  await db
+    .delete(userAllergies)
+    .where(
+      and(eq(userAllergies.userId, userId), eq(userAllergies.allergy, allergy)),
+    );
+}
---
+++ packages/api/src/preferences/services.ts
@@ -0,0 +1,58 @@
+import { upsert } from "@api/utils";
+import type { Drizzle, InsertAllergy, SelectAllergy } from "@peterplate/db";
+import { userDietaryPreferences, users } from "@peterplate/db";
+import { TRPCError } from "@trpc/server";
+import type { InsertPreference } from "@zotmeal/db/src/schema/userDietaryPreferences";
+import { and, eq } from "drizzle-orm";
+
+/**
+ * Get all user dietary preferences for a given user ID.
+ * Returns empty array if the user has no listed preferences.
+ */
+export async function getDietaryPreferences(db: Drizzle, userId: string) {
+  const preferences = await db.query.userDietaryPreferences.findMany({
+    where: (ud, { eq }) => eq(ud.userId, userId),
+  });
+  //Return each allergy string
+  return preferences.map((p) => p.preference);
+}
+
+export async function addDietaryPreference(
+  db: Drizzle,
+  userId: string,
+  preference: string,
+): Promise<InsertPreference> {
+  //check if allergy already in table
+  const existing_preference = await db.query.userDietaryPreferences.findFirst({
+    where: (ud, { eq }) =>
+      and(eq(ud.userId, userId), eq(ud.preference, preference)),
+  });
+
+  if (existing_preference) {
+    return null;
+  }
+
+  const newPreference: InsertPreference = {
+    userId,
+    preference,
+  };
+
+  await db.insert(userDietaryPreferences).values(newPreference);
+
+  return newPreference;
+}
+
+export async function deletePreference(
+  db: Drizzle,
+  userId: string,
+  preference: string,
+): Promise<void> {
+  await db
+    .delete(userDietaryPreferences)
+    .where(
+      and(
+        eq(userDietaryPreferences.userId, userId),
+        eq(userDietaryPreferences.preference, preference),
+      ),
+    );
+}
