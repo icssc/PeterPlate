b1147684c0509f7b2df91cbf136b75c0f396103b
Yitong Liu <yitonl21@uci.edu>
2026-02-14 02:57:11 -0800

    feat: ✨ added extra sapcing for mobile view so toolbar doesn't overlap content

--- apps/next/src/app/layout-client.tsx
+++ apps/next/src/app/layout-client.tsx
@@ -68,7 +68,8 @@ export function RootClient({ children }: { children: React.ReactNode }) {
         <QueryClientProvider client={queryClient}>
           <DateProvider>
             <Toolbar />
-            {children}
+            {/* Extra spacing for mobile view so toolbar doesn't overlap content */}
+            <main className="pb-20 md:pb-0">{children}</main>
           </DateProvider>
         </QueryClientProvider>
       </trpc.Provider>
