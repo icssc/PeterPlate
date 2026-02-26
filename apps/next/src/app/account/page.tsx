f102a3bdcd8b132835bdd85f74fce53e7bbda474
Mihai Zecheru <zecheruchris@gmail.com>
2026-02-26 00:15:52 -0800

    fix: 🐛 remove unnecessary page
    Created this page as a result of miscommunication. Now removed it.

--- apps/next/src/app/account/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import EditPreferencesContent from "@/components/ui/edit-preferences-content";
-
-const AccountPage = () => {
-  return <EditPreferencesContent />;
-};
-
-export default AccountPage;
