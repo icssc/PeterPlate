8084181d37fee8e32e1609401003de6dcc2b01c1
SAMIKA BHAVESH BHATKAR <samikabhatkar@gmail.com>
2026-03-03 16:06:34 -0800

    feat(fix): ✨ 🐛 changed protein default to 100 g

--- packages/db/src/schema/userGoals.ts
+++ packages/db/src/schema/userGoals.ts
@@ -6,7 +6,7 @@ export const userGoals = pgTable("user_goals", {
     .primaryKey()
     .references(() => users.id, { onDelete: "cascade" }),
   calorieGoal: integer("calorie_goal").notNull().default(2000),
-  proteinGoal: integer("protein_goal").notNull().default(75),
+  proteinGoal: integer("protein_goal").notNull().default(100),
   carbGoal: integer("carb_goal").notNull().default(250),
   fatGoal: integer("fat_goal").notNull().default(50),
 });
