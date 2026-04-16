e4b75a45c08ee5b93c82fd0a31bb279085c6bb8a
Jacob Moy <apixelatedpug@gmail.com>
2026-04-16 11:49:39 -0700

    refactor!: 💥 ♻️ drop unnecessary tables, alter dishes

---
+++ packages/db/migrations/0007_peterplate.sql
@@ -0,0 +1,26 @@
+ALTER TABLE "events" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "dishes_to_menus" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "menus" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "nutrition_infos" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "periods" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "restaurants" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "stations" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+DROP TABLE "events" CASCADE;--> statement-breakpoint
+DROP TABLE "dishes_to_menus" CASCADE;--> statement-breakpoint
+DROP TABLE "menus" CASCADE;--> statement-breakpoint
+DROP TABLE "nutrition_infos" CASCADE;--> statement-breakpoint
+DROP TABLE "periods" CASCADE;--> statement-breakpoint
+DROP TABLE "restaurants" CASCADE;--> statement-breakpoint
+DROP TABLE "stations" CASCADE;--> statement-breakpoint
+ALTER TABLE "dishes" DROP CONSTRAINT "dishes_station_id_stations_id_fk";
+--> statement-breakpoint
+DROP INDEX "dishes_station_id_idx";--> statement-breakpoint
+DROP INDEX "dishes_name_idx";--> statement-breakpoint
+DROP INDEX "dishes_category_idx";--> statement-breakpoint
+ALTER TABLE "dishes" DROP COLUMN "station_id";--> statement-breakpoint
+ALTER TABLE "dishes" DROP COLUMN "name";--> statement-breakpoint
+ALTER TABLE "dishes" DROP COLUMN "description";--> statement-breakpoint
+ALTER TABLE "dishes" DROP COLUMN "ingredients";--> statement-breakpoint
+ALTER TABLE "dishes" DROP COLUMN "category";--> statement-breakpoint
+ALTER TABLE "dishes" DROP COLUMN "image_url";--> statement-breakpoint
+ALTER TABLE "dishes" DROP COLUMN "num_ratings";
--- packages/db/src/schema/dishes.ts
+++ packages/db/src/schema/dishes.ts
@@ -1,65 +1,13 @@
-import { relations } from "drizzle-orm";
-import { index, integer, pgTable, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, text } from "drizzle-orm/pg-core";
 
-import {
-  dietRestrictions,
-  type InsertDietRestriction,
-} from "./dietRestrictions";
-import { dishesToMenus } from "./menus";
-import { type InsertNutritionInfo, nutritionInfos } from "./nutritionInfos";
-import { stations } from "./stations";
 import { metadataColumns } from "./utils";
 
-export const dishes = pgTable(
-  "dishes",
-  {
-    id: text("id").primaryKey(),
-    stationId: text("station_id")
-      .notNull()
-      .references(() => stations.id, {
-        onDelete: "restrict",
-        onUpdate: "cascade",
-      }),
-    name: text("name").notNull(),
-    description: text("description").notNull(),
-    ingredients: text("ingredients").default(
-      "Ingredient Statement Not Available",
-    ),
-
-    /** Defaults to "Other" if not specified. */
-    category: text("category").notNull().default("Other"),
-    image_url: text("image_url"),
-    numRatings: integer("num_ratings").default(0).notNull(),
-    totalRating: integer("total_rating").default(0).notNull(),
-    ...metadataColumns,
-  },
-  (table) => ({
-    stationIdx: index("dishes_station_id_idx").on(table.stationId),
-    nameIdx: index("dishes_name_idx").on(table.name),
-    categoryIdx: index("dishes_category_idx").on(table.category),
-  }),
-);
-
-export const dishRelations = relations(dishes, ({ one, many }) => ({
-  dishesToMenus: many(dishesToMenus),
-  station: one(stations, {
-    fields: [dishes.stationId],
-    references: [stations.id],
-  }),
-  dietRestriction: one(dietRestrictions, {
-    fields: [dishes.id],
-    references: [dietRestrictions.dishId],
-  }),
-  nutritionInfo: one(nutritionInfos, {
-    fields: [dishes.id],
-    references: [nutritionInfos.dishId],
-  }),
-}));
+export const dishes = pgTable("dishes", {
+  id: text("id").primaryKey(),
+  totalRating: integer("total_rating").default(0).notNull(),
+  ...metadataColumns,
+});
 
 /** A dish at a restaurant. */
 export type InsertDish = typeof dishes.$inferInsert;
 export type SelectDish = typeof dishes.$inferSelect;
-export interface InsertDishWithRelations extends InsertDish {
-  dietRestriction: InsertDietRestriction;
-  nutritionInfo: InsertNutritionInfo;
-}
