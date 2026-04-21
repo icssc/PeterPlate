81de149c16f05a881f0512a4efa9ba2144179212
Mihai Zecheru <zecheruchris@gmail.com>
2026-04-21 15:49:05 -0700

    feat: ✨ increase content width to match figma

--- apps/next/src/app/about/page.tsx
+++ apps/next/src/app/about/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
+import { GitHub, Language } from "@mui/icons-material";
 import Image from "next/image";
 import Contributor from "@/components/ui/contributor";
 import { Button } from "@/components/ui/shadcn/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/shadcn/dialog";
 import { trpc } from "@/utils/trpc";
 
 export default function About() {
@@ -31,26 +24,16 @@ export default function About() {
         height={2000}
       />
       <div
-        className="flex flex-col md:flex-row p-8 gap-6 justify-around"
+        className="flex flex-col md:flex-row p-8 gap-6 max-w-100"
         id="about-content"
       >
         <div className="flex flex-col" id="about-text">
           <div className="flex gap-4 items-center mb-2" id="about-header">
             <h1 className="text-3xl font-bold" id="about-title">
-              About PeterPlate
+              About <span className="text-sky-700">PeterPlate</span>
             </h1>
-            <Image
-              src="/peterplate-icon.webp"
-              alt="PeterPlate's logo"
-              width={32}
-              height={32}
-              className="rounded-full"
-            />
           </div>
-          <div
-            id="about-paragraph"
-            className="grid gap-4 max-w-3xl max-md:max-w-2xl"
-          >
+          <div id="about-paragraph" className="grid gap-4 max-w-100">
             <p>
               PeterPlate is your go-to app for everything dining at UCI! From
               up-to-date menus and nutritional information to dining hall events
@@ -67,9 +50,40 @@ export default function About() {
               >
                 ICS Student Council
               </a>
-              . As one of ICSSC's smaller teams, we're a tight-knit group
-              passionate about improving student life through technology.
+              , UCI's premier computer science club. As one of ICSSC's smaller
+              teams, we're a tight-knit group passionate about improving student
+              life through technology.
             </p>
+            <div className="flex flex-col gap-4" id="contributors">
+              <h1 className="text-xl max-md:text-base max-sm:text-sm font-bold">
+                Our Lovely Contributors
+              </h1>
+              <div className="flex flex-wrap gap-2" id="contributor-grid">
+                {isLoading && (
+                  <p className="text-sm text-zinc-400">
+                    Loading contributors...
+                  </p>
+                )}
+                {!isLoading && isError && (
+                  <p className="text-sm text-red-500 text-center">
+                    Error occurred while fetching contributors: {error?.message}
+                  </p>
+                )}
+                {!isLoading &&
+                  !isError &&
+                  queryResponse &&
+                  queryResponse.map((contributor) => (
+                    <Contributor
+                      key={`${contributor.login}`}
+                      name={contributor.name || contributor.login}
+                      username={contributor.login}
+                      profileSrc={contributor.avatar_url}
+                      bio={contributor.bio || "PeterPlate Contributor"}
+                      contributions={contributor.contributions}
+                    />
+                  ))}
+              </div>
+            </div>
             <p>
               Want to contribute? PeterPlate is open-source, and we welcome
               contributions on our&nbsp;
@@ -89,76 +103,33 @@ export default function About() {
               </a>
               !
             </p>
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button className="w-fit">Privacy Policy</Button>
-              </DialogTrigger>
-              <DialogContent className="w-md h-auto">
-                <DialogHeader>
-                  <DialogTitle className="p-4 text-center">
-                    Privacy Policy
-                  </DialogTitle>
-                  <DialogDescription asChild className="p-4">
-                    <div>
-                      <p className="mb-4">
-                        PeterPlate is a cross-platform mobile application
-                        designed to help users view dining hall menus at the
-                        University of California, Irvine (UCI). We value your
-                        privacy and are committed to protecting any personal
-                        information you may share with us.
-                      </p>
-                      <p className="mb-4">
-                        PeterPlate does not collect or store any personally
-                        identifiable information. The app does not require login
-                        or account creation. We do not track or monitor user
-                        behavior within the app.
-                      </p>
-                      <p className="mb-4">
-                        PeterPlate fetches dining hall menu data from publicly
-                        available or authorized UCI resources. This data is used
-                        solely to display daily menus within the app and is not
-                        shared or stored beyond your device.
-                      </p>
-                    </div>
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
+            <div className="flex align-items gap-4">
+              <Button
+                className="bg-sky-700"
+                onClick={() =>
+                  window.open(
+                    "https://github.com/icssc/PeterPlate/",
+                    "_blank",
+                    "noopener,noreferrer",
+                  )
+                }
+              >
+                <GitHub /> GitHub
+              </Button>
+              <Button
+                className="bg-sky-700"
+                onClick={() =>
+                  window.open(
+                    "https://studentcouncil.ics.uci.edu/",
+                    "_blank",
+                    "noopener,noreferrer",
+                  )
+                }
+              >
+                <Language /> Visit ICSSC
+              </Button>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col items-center gap-4" id="contributors">
-          <h1 className="text-xl max-md:text-base max-sm:text-sm font-bold">
-            Our Lovely Contributors
-          </h1>
-          <div
-            className="flex flex-wrap justify-center gap-2 max-w-xs"
-            id="contributor-grid"
-          >
-            {isLoading && (
-              <p className="text-sm text-zinc-400">Loading contributors...</p>
-            )}
-            {!isLoading && isError && (
-              <p className="text-sm text-red-500 text-center">
-                Error occurred while fetching contributors: {error?.message}
-              </p>
-            )}
-            {!isLoading &&
-              !isError &&
-              queryResponse &&
-              queryResponse.map((contributor) => (
-                <Contributor
-                  key={`${contributor.login}`}
-                  name={contributor.name || contributor.login}
-                  username={contributor.login}
-                  profileSrc={contributor.avatar_url}
-                  bio={contributor.bio || "PeterPlate Contributor"}
-                  contributions={contributor.contributions}
-                />
-              ))}
-          </div>
-          <p className="text-sm italic font-light text-zinc-500">
-            .. you could be here!
-          </p>
         </div>
       </div>
     </div>
