17646385f1c3b53f63776a94a223393eda9a95df
Jacob Moy <apixelatedpug@gmail.com>
2026-04-28 11:47:17 -0700

    fix(migration): 🐛 remove nulls from ratings table's restaurant field

--- packages/db/migrations/0011_peterplate.sql
+++ packages/db/migrations/0011_peterplate.sql
@@ -1,1 +1,2 @@
-ALTER TABLE "ratings" ADD COLUMN "restaurant" "restaurant_id_enum" NOT NULL;
+ALTER TABLE "ratings" ADD COLUMN "restaurant" "restaurant_id_enum" DEFAULT 'anteatery' NOT NULL;--> statement-breakpoint
+ALTER TABLE "ratings" ALTER COLUMN "restaurant" DROP DEFAULT;
